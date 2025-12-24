14e80de395919c75148cb860155bf2a6441a2aca
chuxin <3970987337@qq.com>
2025-12-24 15:08:14 +0800

    feat: 集成 jessibuca 播放器

--- Video/xgplayer3/src/App.vue
+++ Video/xgplayer3/src/App.vue
@@ -1,20 +1,41 @@
 <template>
-  <div>
+  <div class="app-container">
     <div id="mse"></div>
+    <APlayer />
   </div>
 </template>
 
 <script setup lang="ts">
+import APlayer from "./a.vue"
 import Player from 'xgplayer';
 import FlvPlugin from 'xgplayer-flv'
 import 'xgplayer/dist/index.min.css';
 
-let player = new Player({
-  id: 'mse',
-  url: 'http://sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/flv/xgplayer-demo-720p.flv',
-  height: '100%',
-  width: '100%',
-  plugins: [FlvPlugin],
-});
+setTimeout(() => {
+  let player = new Player({
+    id: 'mse',
+    // url: 'http://sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/flv/xgplayer-demo-720p.flv',
+    url: '../public/output_aac.flv',
+    height: '200px',
+    width: '200px',
+    plugins: [FlvPlugin],
+  });
 
+
+}, 3000);
 </script>
+
+
+<style>
+html,
+#app,
+body {
+  height: 100vh;
+  height: 100vw;
+}
+
+.app-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ Video/xgplayer3/src/a.vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="width: 200px;height: 200px;background-color: #000;">
+    <div id="app1"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+  setTimeout(()=>{
+   console.log(window.Jessibuca)
+  const player =  new window.Jessibuca({
+          container: document.getElementById("app1"),
+          isFlv: true,
+          videoBuffer: 0.2, // 缓冲时长
+          isResize: false, // 是否自动调整大小
+          hasAudio: true, // 是否有音频
+          loadingText: '加载中...',
+          showBandwidth: false,
+          loadingTimeout: 10,
+          heartTimeout: 10,
+          heartTimeoutReplay: false,
+          loadingTimeoutReplay: false,
+          supportDblclickFullscreen: true,
+          operateBtns: {
+            fullscreen: true, // 全屏按钮
+            screenshot: true, // 截图按钮
+            play: true, // 播放/暂停按钮
+            audio: true, // 音频按钮
+          },
+        });
+
+      // ../public/output_aac.flv
+        player.play('http://sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/flv/xgplayer-demo-720p.flv').catch((err) => {
+    console.log(err);
+  })
+  },3000)
+
+</script>
