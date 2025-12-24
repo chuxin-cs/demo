d2454ceb2773fb34df205fa989c52381503a865d
chuxin <3970987337@qq.com>
2025-12-24 14:38:51 +0800

    feat: 项目初始化

---
+++ Video/xgplayer3/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div id="mse"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Player from 'xgplayer';
+import FlvPlugin from 'xgplayer-flv'
+import 'xgplayer/dist/index.min.css';
+
+let player = new Player({
+  id: 'mse',
+  url: 'http://sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/flv/xgplayer-demo-720p.flv',
+  height: '100%',
+  width: '100%',
+  plugins: [FlvPlugin],
+});
+
+</script>
