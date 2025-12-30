0076e673f5e6aa9f1968dd2dbdcbeb1455af4d73
chuxin <3970987337@qq.com>
2025-12-30 16:34:01 +0800

    feat(backend): 添加Swagger文档支持和knife4j增强UI
    添加@fastify/static和nestjs-knife4j-plus依赖以支持Swagger文档生成
    在main.ts中配置Swagger文档和knife4j增强UI界面

--- Node/backend-template/src/main.ts
+++ Node/backend-template/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { knife4jSetup } from 'nestjs-knife4j-plus';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -15,6 +17,22 @@ async function bootstrap() {
   );
   // 全局序列化拦截器，支持 @Exclude 隐藏敏感字段
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('swagger example')
+    .setDescription('The swagger API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await knife4jSetup(app, [
+    {
+      name: '2.0 version', // 文档版本名称
+      url: `/api2`, // Swagger openapi JSON地址
+    },
+  ]);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
